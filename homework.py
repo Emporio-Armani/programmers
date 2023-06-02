e6f0bcdc2d47e7d38628e0fdb939c270210c8e75
darinavaleeva21 <124895834+darinavaleeva21@users.noreply.github.com>
2023-06-02 19:17:54 +0300

    Update homework.py
    Сделала сообщение приветствия бота

--- homework.py
+++ homework.py
@@ -3,3 +3,8 @@ TOKEN = 'your_token'
 
 # создание экземпляра бота
 bot = telebot.TeleBot(TOKEN)
+
+# сообщение приветствия
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Привет! Я твой новый телеграм-бот :)")
