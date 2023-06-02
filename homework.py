c0ddca01832471557173d2a6481e6efbd1a98bfc
PaninaTanya <124932134+PaninaTanya@users.noreply.github.com>
2023-06-02 19:27:14 +0300

    Update homework.py
    Сделала команду для запуска бота

--- homework.py
+++ homework.py
@@ -1,16 +1,19 @@
-# получение токена бота из BotFather
+# Получение токена бота из BotFather
 TOKEN = 'your_token'
 
-# создание экземпляра бота
+# Создание экземпляра бота
 bot = telebot.TeleBot(TOKEN)
 
-# сообщение приветствия
+# Сообщение приветствия
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.chat.id, "Привет! Я твой новый телеграм-бот :)")
 
-# ответ на вопрос "как дела?"
+# Ответ на вопрос "как дела?"
 @bot.message_handler(func=lambda message: message.text == 'как дела?')
 def how_are_you(message):
     answers = ['Отлично, спасибо!', 'Неплохо, а у тебя?', 'Бывало и лучше, но пока неплохо :)']
     bot.send_message(message.chat.id, random.choice(answers))
+
+# Запуск бота
+bot.polling()
