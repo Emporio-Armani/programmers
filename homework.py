ca9a566876194dfccf937f6eb9bbcc78aadfeff4
Emporio-Armani <114580813+Emporio-Armani@users.noreply.github.com>
2023-06-02 19:21:27 +0300

    Update homework.py
    Сделал варианты ответы на вопрос "Как дела?"

--- homework.py
+++ homework.py
@@ -8,3 +8,9 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.chat.id, "Привет! Я твой новый телеграм-бот :)")
+
+# ответ на вопрос "как дела?"
+@bot.message_handler(func=lambda message: message.text == 'как дела?')
+def how_are_you(message):
+    answers = ['Отлично, спасибо!', 'Неплохо, а у тебя?', 'Бывало и лучше, но пока неплохо :)']
+    bot.send_message(message.chat.id, random.choice(answers))
